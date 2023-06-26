221813ec33d21e8c8fc5eae252f270eacbf22f90
Harsha Murupudi <itsmeeeharsha@gmail.com>
2023-06-26 01:38:56 -0500

    ADD: Group feature to operations

--- src/routes/operation.ts
+++ src/routes/operation.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import fs from 'fs';
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 const Operation = require('../models/Operation');
 const router = express.Router();
 const { glDB } = require('../config/database');
@@ -9,14 +11,24 @@ router.get('/operations/:jobID', async (req: Request, res: Response) => {
     const { jobID } = req.params;
 
     const operations = await Operation.findAll({
-      where: { Job: jobID },
+      where: { Job: { [Op.like]: '%' + jobID + '%' } },
       order: [['Sequence', 'ASC']],
     });
 
+    // console.log(operations);
+    const groupedOperations = operations.reduce((obj: any, item: any) => {
+      if (obj[item.Job]) {
+        obj[item.Job].push(item);
+      } else {
+        obj[item.Job] = [item];
+      }
+
+      return obj;
+    }, {});
+
     res.status(200).json({
       status: 'success',
-      results: operations.length,
-      operations,
+      operations: groupedOperations,
     });
   } catch (error: any) {
     res.status(400).json({
