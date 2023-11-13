35734b3870d5d2a18adb171b40e0e2ba5bb1bbc3
SumitM96 <sumitm@general-label.com>
2023-11-13 11:19:11 -0600

    UPDATE: Added Floor Notes to Operations Query

--- src/models/Operation.ts
+++ src/models/Operation.ts
@@ -31,6 +31,9 @@ const Operation = db.define(
     Note_Text: {
       type: Sequelize.STRING,
     },
+    Floor_Notes: {
+      type: Sequelize.STRING,
+    },
     Est_Total_Hrs: {
       type: Sequelize.NUMBER,
     },
