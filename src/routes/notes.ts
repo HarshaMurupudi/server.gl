995c2c057978e0f403c1651c80e9759159b5f463
SumitM96 <sumitm@general-label.com>
2023-11-21 16:02:47 -0600

    FIX: Fixed Sequelize patch for Meeting Page

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -151,7 +151,6 @@ router.patch("/meeting/notes", async (req, res) => {
       Date = null,
       Meeting_Note = null,
     } of meetings) {
-      const values = { Description, Date, Meeting_Note };
       const obj = await MeetingNotes.findOne({
         where: { Meeting_Note_ID },
       });
@@ -164,7 +163,9 @@ router.patch("/meeting/notes", async (req, res) => {
       } else {
         MeetingNotes.create({
           Meeting_Note_ID,
-          values
+          Description,
+          Date,
+          Meeting_Note
         });
       }
     }
