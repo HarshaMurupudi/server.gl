e369ef0a35b0ee38efac87b65f81656d0688b746
generallabel2 <generallabel2@gmail.com>
2023-12-14 12:33:09 -0600

    FIX: Data format

--- src/routes/calendar.ts
+++ src/routes/calendar.ts
@@ -1,3 +1,4 @@
+import { count } from "console";
 import express from "express";
 const { log } = require("console");
 const { glDB } = require("../config/database");
@@ -26,91 +27,106 @@ router.get("/attendance/events", async (req, res) => {
         AND t2.Work_Date > dateadd(WEEK, -2, GETDATE())
         AND t2.Work_Date < dateadd(MONTH, 6, GETDATE())
     `);
-    
+
     // const holidays = await glDB.query(`
     //       SELECT
     // `);
 
     interface Event {
-        Employee: string;
-        Status: string;
-        First_Name: string;
-        Last_Name: string;
-        Work_Date: string;
-        Attendance_Type: number;
-      }
-      
+      Employee: string;
+      Status: string;
+      First_Name: string;
+      Last_Name: string;
+      Work_Date: string;
+      Attendance_Type: number;
+    }
 
-    const formatData = (data: { status: string; results: number; events: Event[] }) => {
-        const formattedEvents = [];
-      
-        // group events by employee
-        const groupedEvents: { [key: string]: Event[] } = events[0].reduce((acc: any, event: any) => {
+    const formatData = (data: {
+      status: string;
+      results: number;
+      events: Event[];
+    }) => {
+      const formattedEvents = [];
+
+      // group events by employee
+      const groupedEvents: { [key: string]: Event[] } = events[0].reduce(
+        (acc: any, event: any) => {
           const key = `${event.Employee}-${event.Attendance_Type}`;
           if (!acc[key]) {
             acc[key] = [];
           }
           acc[key].push(event);
           return acc;
-        }, {});
-      
-        // handle grouped events
-        for (const key in groupedEvents) {
-          const events = groupedEvents[key].sort((a, b) => new Date(a.Work_Date).getTime() - new Date(b.Work_Date).getTime());
-      
-          let currentStartDate = null;
-          let currentEndDate = null;
-      
-          for (const event of events) {
-            const eventDate = new Date(event.Work_Date).getTime();
-      
-            // check if the current event is consecutive to the last one
-            if (currentEndDate && eventDate - currentEndDate === 24 * 60 * 60 * 1000) {
+        },
+        {}
+      );
+
+      let eventCount = 0;
+      // handle grouped events
+      for (const key in groupedEvents) {
+        const events = groupedEvents[key].sort(
+          (a, b) =>
+            new Date(a.Work_Date).getTime() - new Date(b.Work_Date).getTime()
+        );
+
+        let currentStartDate = null;
+        let currentEndDate = null;
+
+        for (const event of events) {
+          const eventDate = new Date(event.Work_Date).getTime();
+
+          // check if the current event is consecutive to the last one
+          if (
+            currentEndDate &&
+            eventDate - currentEndDate === 24 * 60 * 60 * 1000
+          ) {
             currentEndDate = eventDate;
-            } else {
+          } else {
             if (currentStartDate && currentEndDate) {
-                const fullName = `${event.First_Name} ${event.Last_Name}`;
-                let title = '';
-                if (event.Attendance_Type === 2) {
-                    title = `${fullName} - Vacation`;
-                } else {
-                    title = `${fullName} - Sick`;
-                }
-                const formattedEvent = {
+              const fullName = `${event.First_Name} ${event.Last_Name}`;
+              let title = "";
+              if (event.Attendance_Type === 2) {
+                title = `${fullName} - Vacation`;
+              } else {
+                title = `${fullName} - Sick`;
+              }
+              const formattedEvent = {
+                id: eventCount,
                 title,
                 allDay: true,
                 start: new Date(currentStartDate),
                 end: new Date(currentEndDate),
-                };
-    
-                formattedEvents.push(formattedEvent);
+              };
+
+              formattedEvents.push(formattedEvent);
             }
-    
+
             // start a new consecutive vacation period
             currentStartDate = eventDate;
             currentEndDate = eventDate;
-            }
-          }
-      
-          // add the last consecutive event for the employee, if any
-          if (currentStartDate && currentEndDate) {
-            const fullName = `${events[0].First_Name} ${events[0].Last_Name}`;
-            const title = `${fullName} - Vacation`;
-      
-            const formattedEvent = {
-              title,
-              allDay: true,
-              start: new Date(currentStartDate),
-              end: new Date(currentEndDate),
-            };
-      
-            formattedEvents.push(formattedEvent);
           }
         }
-      
-        return formattedEvents;
-      };
-      
+
+        // add the last consecutive event for the employee, if any
+        if (currentStartDate && currentEndDate) {
+          const fullName = `${events[0].First_Name} ${events[0].Last_Name}`;
+          const title = `${fullName} - Vacation`;
+
+          const formattedEvent = {
+            id: eventCount,
+            title,
+            allDay: true,
+            start: new Date(currentStartDate),
+            end: new Date(currentEndDate),
+          };
+
+          formattedEvents.push(formattedEvent);
+        }
+        eventCount++;
+      }
+
+      return formattedEvents;
+    };
 
     events = formatData(events);
 
