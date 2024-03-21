68e3c79b890250e9fd888e5f4584291a69c9552c
Harsha Murupudi <itsmeeeharsha@gmail.com>
2024-03-21 10:45:33 +0530

    FIX: Mutiple shipline not showing issue in wc queues

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,28 @@
 module.exports = {
-    apps: [
-        {
-            name: 'app1',
-            script: './build/index.js',
-            env_production: {
-                NODE_ENV: 'production',
-            },
-            env_development: {
-                NODE_ENV: 'development'
-            }
-        },
-        {
-            name: 'app2',
-            script: './build/taskRunner.js',
-            env_production: {
-                NODE_ENV: 'production',
-            },
-            env_development: {
-                NODE_ENV: 'development'
-            }
-        },
-
-    ]
-}
+  apps: [
+    {
+      name: 'app1',
+      script: './build/index.js',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
+      'out_file': '/dev/null',
+      'error_file': '/dev/null',
+    },
+    {
+      name: 'app2',
+      script: './build/taskRunner.js',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      env_development: {
+        NODE_ENV: 'development',
+      },
+      'out_file': '/dev/null',
+      'error_file': '/dev/null',
+    },
+  ],
+};
