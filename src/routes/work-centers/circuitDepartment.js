b893e8a252456b924eaa2356096f5553c144f0d0
Harsha Murupudi <itsmeeeharsha@gmail.com>
2024-03-26 12:48:35 +0530

    FIX: Two entries in work center queues

--- src/routes/work-centers/circuitDepartment.js
+++ src/routes/work-centers/circuitDepartment.js
@@ -52,7 +52,7 @@ router.get('/circuit/jobsByWorkCenter/:workCenterName', async (req, res) => {
       const fJobs = await glDB.query(
         `
         SELECT * FROM (
-          SELECT	*, ROW_NUMBER() OVER (PARTITION BY Promised_Date ORDER BY Sched_Start) AS t_row_number
+          SELECT	*, ROW_NUMBER() OVER (PARTITION BY Promised_Date ORDER BY Promised_Date) AS t_row_number
           FROM (
             SELECT j.[Job], [Part_Number], [Customer], j.[Status], j.[Description], [Order_Quantity], [Completed_Quantity], [Released_Date], 
             j.Sched_Start, j.Make_Quantity, j.Note_Text, j.Sales_Code, jo.Work_Center, j.Rev, j.Quote,
@@ -91,7 +91,7 @@ router.get('/circuit/jobsByWorkCenter/:workCenterName', async (req, res) => {
             WHERE j.[Job] IN (:jobIDs) AND jo.Work_Center = :wc
           ) AS t
             WHERE t.row_number = 1 OR t.row_number = 2) AS t2
-        WHERE t2.t_row_number = 1 OR t2.row_number = 1;
+        WHERE row_number = 1;
           `,
         {
           replacements: {
